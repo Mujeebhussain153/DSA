49e7cec8f9493e4a41cdb3f4c38290e198b9a4e3
Mujeebhussain153 <mujeebhussain153@gmail.com>
2025-05-20 10:48:29 +0530

    main: Made minute changes to the checkSorted.py file

--- Arrays/checkSorted.py
+++ Arrays/checkSorted.py
@@ -1,14 +1,14 @@
-# This Function is to check whether the array is sorted or not
+# This Function is to check whether the array is sorted in ascending Order or not
 def isSorted(nums):
     try:
-        largest = -1
         n = len(nums)
-        for i in range(n):
-            if(nums[i] > largest):
-                largest = nums[i]
-        return largest == nums[-1]
+        for i in range(1, n):
+            if(nums[i-1] > nums[i]):
+                return False
+        return True
     except Exception as e:
         print(e)
         return
 
-print(isSorted([20, 20, 78, 98, 99, 97]))
+print(isSorted([2, 11, 45, 99, 154]))
+print(isSorted([2, 78, 45, 99, 154]))
