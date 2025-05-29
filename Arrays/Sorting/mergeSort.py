22077fb97ee18210e8801249f8f3437a0dedd8a1
Mujeebhussain153 <mujeebhussain153@gmail.com>
2025-05-29 13:03:56 +0530

    Added a file where the mergeSort algorithm is written

---
+++ Arrays/Sorting/mergeSort.py
@@ -0,0 +1,31 @@
+def mergeSort(arr):
+    try:
+        if(len(arr) <=1):
+            return arr
+        mid = len(arr)//2
+        left = mergeSort(arr[:mid])
+        right = mergeSort(arr[mid:])
+        return merge(left, right)
+    except Exception as e:
+        print(e)
+        return
+
+def merge(left, right):
+    try:
+        merged = []
+        i, j = 0, 0
+        while(i< len(left) and j < len(right)):
+            if(left[i] <= right[j]):
+                merged.append(left[i])
+                i+=1
+            else:
+                merged.append(right[j])
+                j+=1
+        merged+=left[i:]
+        merged+=right[j:]
+        return merged
+    except Exception as e:
+        print(e)
+        return
+
+print(mergeSort([5,2,4,3,1]))
