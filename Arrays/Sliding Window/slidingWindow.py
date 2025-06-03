6b967fe7ea0e1f311d23ad17d5635dbdbcaf62c9
Mujeebhussain153 <mujeebhussain153@gmail.com>
2025-06-03 18:16:54 +0530

    Implemented Sliding Window Technique where the max sum of subarray can be found for specific length of subarray

---
+++ Arrays/Sliding Window/slidingWindow.py
@@ -0,0 +1,16 @@
+def maximumSubarraySum(arr, k):
+    try:
+        n = len(arr)
+        if(n<=k):
+            return -1
+        window_sum = sum(arr[0:k])
+        max_sum = window_sum
+        for i in range(k, n):
+            window_sum = window_sum - arr[i-k]+arr[i]
+            max_sum = window_sum if window_sum > max_sum else max_sum
+        return max_sum
+    except Exception as e:
+        print(e)
+        return
+
+print(maximumSubarraySum([1, 4, 2, 10, 2, 3, 1, 0, 20], 3))
